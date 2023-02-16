ad8209a4624fc632ab08b038d4c12859a701006e
Tulio Macedo <tuliomac@gmail.com>
2023-02-16 01:02:40 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
