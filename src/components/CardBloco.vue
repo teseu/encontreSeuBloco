d8dd21541e04991afb60d2bd077c41df0cea7ac8
Tulio Macedo <tuliomac@gmail.com>
2023-02-17 01:30:15 -0300

    iniciando os blocos em si ✨

--- src/components/CardBloco.vue
+++ src/components/CardBloco.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <img src="../assets/img/bloco1.png" alt="man with a top hat">
-    <div class="block__title">O Python do vovô não sobe mais</div>
-    <div class="block__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis fugit earum iusto assumenda repellendus laboriosam atque molestias magni totam, suscipit, saepe temporibus voluptatibus exercitationem tempora recusandae, neque cupiditate praesentium cumque.</div>
-    <i></i>
-    <div class="location">São Paulo - SP</div>
-  </div>
+	<div>
+		<img src="../assets/img/bloco1.png" alt="man with a top hat" />
+		<div class="block__title">O Python do vovô não sobe mais</div>
+		<div class="block__desc">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis fugit earum
+			iusto assumenda repellendus laboriosam atque molestias magni totam,
+			suscipit, saepe temporibus voluptatibus exercitationem tempora recusandae,
+			neque cupiditate praesentium cumque.
+		</div>
+		<i></i>
+		<div class="location">São Paulo - SP</div>
+	</div>
 </template>
